26128d4559349f82402e783dacb214f360a8571c
Simon Johansson <simonjohansson312@gmail.com>
2022-02-11 10:57:37 +0100

    React with skulpt, altough many errors

--- src/App.js
+++ src/App.js
@@ -1,62 +1,83 @@
-import React from "react";
-import "./App.css";
-import Code_box from "./Components/CodeBox";
-import Header from "./Components/Header";
-import Output_box from "./Components/OutputBox";
-import Visual_box from "./Components/VisualBox";
-import Control_panel from "./Components/ControlPanel";
+
+import React from 'react';
+import { useState } from "react";
+
+class CodeBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "import turtle\nt = turtle.Turtle()\nfor c in ['red', 'green', 'yellow', 'blue']:\n\tt.color(c)\n\tt.forward(75)\n\tt.left(90)",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    runit(this.state.value);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <textarea value={this.state.value} onChange={this.handleChange} cols="50" rows="20" />
+        </label>
+        <input type="submit" value="Run" />
+        
+      </form>
+
+    );
+  }
+}
+
+function builtinRead(x) {
+  if (window.Sk.builtinFiles === undefined || window.Sk.builtinFiles["files"][x] === undefined)
+          throw "File not found: '" + x + "'";
+  return window.Sk.builtinFiles["files"][x]
+ 
+}
+
+function outf(text) { 
+  var mypre = document.getElementById("output"); 
+  mypre.innerHTML = mypre.innerHTML + text; 
+} 
+
+function runit(prog, output) { 
+  window.Sk.global.console.log("hej");
+  window.Sk.pre = "output";
+  window.Sk.configure({output:outf, read:builtinRead}); 
+  (window.Sk.TurtleGraphics || (window.Sk.TurtleGraphics = {})).target = 'mycanvas';
+  var myPromise = window.Sk.misceval.asyncToPromise(function() {
+      return window.Sk.importMainWithBody("<stdin>", false, prog, true);
+  });
+  myPromise.then(function(mod) {
+      console.log('success');
+  },
+      function(err) {
+      console.log(err.toString());
+  });
+}
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div id="App-body">
-        <div id="Left-body">
-          <Control_panel />
-          <Code_box />
-          <Output_box />
-        </div>
-        <div id="Right-body">
-          <Visual_box />
-        </div>
-      </div>
-    </div>
+    <html> 
+      <body> 
+      <h3>Try This</h3> 
+      <CodeBox id="yourcode" ></CodeBox> 
+      <pre id="output" ></pre> 
+
+      <div id="mycanvas"></div> 
+
+      </body> 
+    </html> 
   );
 }
 
 export default App;
-
-// // output functions are configurable.  This one just appends some text
-// // to a pre element.
-// function outf(text) {
-//   var mypre = document.getElementById("output");
-//   mypre.innerHTML = mypre.innerHTML + text;
-// }
-// function builtinRead(x) {
-//   if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-//           throw "File not found: '" + x + "'";
-//   return Sk.builtinFiles["files"][x];
-// }
-
-// // Here's everything you need to run a python program in skulpt
-// // grab the code from your textarea
-// // get a reference to your pre element for output
-// // configure the output function
-// // call Sk.importMainWithBody()
-// function runit() {
-//  var prog = document.getElementById("yourcode").value;
-//  var mypre = document.getElementById("output");
-//  mypre.innerHTML = '';
-//  Sk.pre = "output";
-//  Sk.configure({output:outf, read:builtinRead});
-//  (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
-//  var myPromise = Sk.misceval.asyncToPromise(function() {
-//      return Sk.importMainWithBody("<stdin>", false, prog, true);
-//  });
-//  myPromise.then(function(mod) {
-//      console.log('success');
-//  },
-//      function(err) {
-//      console.log(err.toString());
-//  });
-// }
