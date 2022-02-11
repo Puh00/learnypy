ffae1043805ee6a7745b5d3cd5a2570f629ff375
Simon Johansson <simonjohansson312@gmail.com>
2022-02-11 12:37:59 +0100

    Restructure

--- src/App.js
+++ src/App.js
@@ -1,83 +1,57 @@
-
 import React from 'react';
-import { useState } from "react";
+import CodeBox from './Components/CodeBox';
+import Header from './Components/Header';
 
-class CodeBox extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "import turtle\nt = turtle.Turtle()\nfor c in ['red', 'green', 'yellow', 'blue']:\n\tt.color(c)\n\tt.forward(75)\n\tt.left(90)",
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    runit(this.state.value);
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <textarea value={this.state.value} onChange={this.handleChange} cols="50" rows="20" />
-        </label>
-        <input type="submit" value="Run" />
-        
-      </form>
-
-    );
-  }
-}
+import Control_panel from './Components/ControlPanel';
+import Visual_box from './Components/VisualBox';
 
 function builtinRead(x) {
-  if (window.Sk.builtinFiles === undefined || window.Sk.builtinFiles["files"][x] === undefined)
-          throw "File not found: '" + x + "'";
-  return window.Sk.builtinFiles["files"][x]
- 
+  if (window.Sk.builtinFiles === undefined || window.Sk.builtinFiles['files'][x] === undefined)
+    throw "File not found: '" + x + "'";
+  return window.Sk.builtinFiles['files'][x];
 }
 
-function outf(text) { 
-  var mypre = document.getElementById("output"); 
-  mypre.innerHTML = mypre.innerHTML + text; 
-} 
+function outf(text) {
+  var mypre = document.getElementById('output');
+  mypre.innerHTML = mypre.innerHTML + text;
+}
 
-function runit(prog, output) { 
-  window.Sk.global.console.log("hej");
-  window.Sk.pre = "output";
-  window.Sk.configure({output:outf, read:builtinRead}); 
+function runit(prog) {
+  window.Sk.global.console.log('hej');
+  window.Sk.pre = 'output';
+  window.Sk.configure({ output: outf, read: builtinRead });
   (window.Sk.TurtleGraphics || (window.Sk.TurtleGraphics = {})).target = 'mycanvas';
-  var myPromise = window.Sk.misceval.asyncToPromise(function() {
-      return window.Sk.importMainWithBody("<stdin>", false, prog, true);
+  var myPromise = window.Sk.misceval.asyncToPromise(function () {
+    return window.Sk.importMainWithBody('<stdin>', false, prog, true);
   });
-  myPromise.then(function(mod) {
+  myPromise.then(
+    function () {
       console.log('success');
-  },
-      function(err) {
+    },
+    function (err) {
       console.log(err.toString());
-  });
+    }
+  );
 }
 
 function App() {
   return (
-    <html> 
-      <body> 
-      <h3>Try This</h3> 
-      <CodeBox id="yourcode" ></CodeBox> 
-      <pre id="output" ></pre> 
+    <div className="App">
+      <Header />
+      <div id="App-body">
+        <div id="Left-body">
+          <Control_panel />
+          <h3>Try This</h3>
+          <CodeBox id="yourcode" runit={runit}></CodeBox>
+          <pre id="output"></pre>
 
-      <div id="mycanvas"></div> 
-
-      </body> 
-    </html> 
+          <div id="mycanvas"></div>
+        </div>
+        <div id="Right-body">
+          <Visual_box />
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default App;
