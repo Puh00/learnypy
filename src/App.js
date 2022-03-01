d75d8f76eadcca67c852c097972b55ca0e7a12ec
Simon Johansson <simonjohansson312@gmail.com>
2022-03-01 15:05:08 +0100

    now plz

--- src/App.js
+++ src/App.js
@@ -6,22 +6,11 @@ import Control_panel from './Components/ControlPanel';
 import VisualBox from './Components/VisualBox';
 import Output_box from './Components/OutputBox';
 
-<<<<<<< HEAD
 import { func, start, step, runit } from './SkulptWrapper/skulptWrapper';
 
 function App() {
   const [refs, setRefs] = useState({ objects: [], variables: [] });
   const [output, setOutput] = useState({ text: '' });
-=======
-import { start, step, runit } from './SkulptWrapper/skulptWrapper';
-
-function App() {
-  const [refs, setRefs] = useState({ objects: [], variables: [] });
-
-  // instantiate with setRefs as the callback function
-  const runit_callback = (prog) => runit(prog, setRefs);
-  const step_callback = (prog) => step(prog, setRefs);
->>>>>>> origin
 
   // instantiate with setRefs as the callback function
   const runit_callback = (prog) => runit(prog, setRefs);
@@ -44,13 +33,8 @@ function App() {
       <div id="App-body">
         <div id="Left-body">
           <Control_panel />
-<<<<<<< HEAD
           <CodeBox runit={runit_callback} restart={start_callback} step={step_callback}></CodeBox>
           <Output_box text={output} />
-=======
-          <CodeBox runit={runit_callback} restart={start} step={step_callback}></CodeBox>
-          <pre id="output" tabIndex={0}></pre>
->>>>>>> origin
         </div>
         <div id="Right-body">
           <VisualBox data={refs} />
