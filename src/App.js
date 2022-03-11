5c3653c223efd9051f5d4bfd92e4a0919e227e5e
yen <54945977+yenanw@users.noreply.github.com>
2022-03-11 22:21:22 +0100

    About page (#20)
    * refactor to use routers and create dummy About page
    
    * add links to about and home
    
    * finish up header
    
    * add some text to the about page
    
    * add link to GitHub and special thanks in the about page
    
    * change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import CodeBox from './Components/CodeBox';
-import Header from './Components/Header';
-import ControlPanel from './Components/ControlPanel';
-import VisualBox from './Components/VisualBox';
-import Output_box from './Components/OutputBox';
 
-import { func, start, step, runit } from './SkulptWrapper/skulptWrapper';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 function App() {
-  const [refs, setRefs] = useState({ objects: [], variables: [] });
-  const [output, setOutput] = useState({ text: '' });
-  const [code, setCode] = useState('a=1\nb=1\nc=b');
-
-  let latest_output = '';
-
-  // instantiate with setRefs as the callback function
-  const runit_callback = (prog) => {
-    setOutput({ test: '' });
-    runit(prog, setRefs);
-    latest_output = '';
-  };
-  const step_callback = (prog) => {
-    setOutput({ text: latest_output });
-    step(prog, setRefs);
-  };
-  const start_callback = (prog) =>
-    start(prog, false, () => {
-      setOutput({ text: '' });
-    });
-  func.outf = (text) => {
-    latest_output = latest_output + text;
-    setOutput({ text: latest_output });
-  };
-
   return (
-    <div className="App">
-      <Header />
-      <div id="App-body">
-        <div id="Left-body">
-          <ControlPanel
-            code={code}
-            runit={runit_callback}
-            step={step_callback}
-            restart={start_callback}
-          />
-          <CodeBox code={code} setCode={setCode} />
-          <Output_box text={output} />
-        </div>
-        <div id="Right-body">
-          <VisualBox data={refs} />
-        </div>
-      </div>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
   );
 }
+
 export default App;
