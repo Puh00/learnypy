5843418e0bcb59fea3ec47aad6389ac18fa12d4a
Simon Johansson <simonjohansson312@gmail.com>
2022-03-17 14:17:15 +0100

    Made functional instead

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -10,57 +10,48 @@ const example1 = 'a=[]\nb=a\nb.append(3)\nprint(b)';
 const example2 = 'a=2\nb=1\nc=b';
 const example3 = 'a=3\nb=1\nc=b';
 
-class DropdownLocal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  // forwardRef again here!
-  // Dropdown needs access to the DOM of the Menu to measure it
-
-  render() {
-    return (
-      <div className={'Control-button'}>
-        <Dropdown autoClose="outside">
-          <Dropdown.Toggle variant="success" id="Dropdown" className={'Dropdown-button'}>
-            {this.props.logo}
-          </Dropdown.Toggle>
-          <Dropdown.Menu as={this.CustomMenu} id={'dropdownMenu'}>
-            <Dropdown.Item as="button" id={'Dropdown-item'} href="#">
-              <button
-                id={'Dropdown-button'}
-                onClick={() => {
-                  this.props.setCode(example1);
-                  this.props.restart(example1);
-                }}>
-                Example 1
-              </button>
-            </Dropdown.Item>
-            <Dropdown.Item as="button" id={'Dropdown-item'} href="#">
-              <button
-                id={'Dropdown-button'}
-                onClick={() => {
-                  this.props.setCode(example2);
-                  this.props.restart(example2);
-                }}>
-                Example 2
-              </button>
-            </Dropdown.Item>
-            <Dropdown.Item as="button" id={'Dropdown-item'} href="#">
-              <button
-                id={'Dropdown-button'}
-                onClick={() => {
-                  this.props.setCode(example3);
-                  this.props.restart(example3);
-                }}>
-                Example 3
-              </button>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-    );
-  }
+function DropdownLocal(props) {
+  return (
+    <div className={'Control-button'}>
+      <Dropdown autoClose="outside">
+        <Dropdown.Toggle variant="success" id="Dropdown" className={'Dropdown-button'}>
+          {props.logo}
+        </Dropdown.Toggle>
+        <Dropdown.Menu id={'dropdownMenu'}>
+          <Dropdown.Item as="button" id={'Dropdown-item'} href="#">
+            <button
+              id={'Dropdown-button'}
+              onClick={() => {
+                props.setCode(example1);
+                props.restart(example1);
+              }}>
+              Example 1
+            </button>
+          </Dropdown.Item>
+          <Dropdown.Item as="button" id={'Dropdown-item'} href="#">
+            <button
+              id={'Dropdown-button'}
+              onClick={() => {
+                props.setCode(example2);
+                props.restart(example2);
+              }}>
+              Example 2
+            </button>
+          </Dropdown.Item>
+          <Dropdown.Item as="button" id={'Dropdown-item'} href="#">
+            <button
+              id={'Dropdown-button'}
+              onClick={() => {
+                props.setCode(example3);
+                props.restart(example3);
+              }}>
+              Example 3
+            </button>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 }
 
 export default DropdownLocal;
