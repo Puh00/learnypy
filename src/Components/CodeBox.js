fec6ab8aab6dce33c4fc0dde5b813c062c7c09fc
verasve <verasv@student.chalmers.se>
2022-03-24 14:44:12 +0100

    markers changed from html to svg

--- src/Components/CodeBox.js
+++ src/Components/CodeBox.js
@@ -28,8 +28,9 @@ const CodeBox = ({ code, setCode, line }) => {
     if (line > 0) {
       // Create line marker
       var marker_node = document.createElement('span');
-      marker_node.innerHTML = '&#11166;';
-      marker_node.style.fontSize = '12px';
+      marker_node.className = 'marker-node';
+      marker_node.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="16" fill="currentColor"><path d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1.828-8L8.293 6.464A1 1 0 0 1 9.707 5.05l4.243 4.243a1 1 0 0 1 0 1.414L9.707 14.95a1 1 0 1 1-1.414-1.414L11.828 10z"></path></svg>';
 
       // highlight the current execution row
       editor.addLineClass(line, 'wrap', 'mark');
@@ -39,8 +40,9 @@ const CodeBox = ({ code, setCode, line }) => {
 
   const setBreakpoint = (lineNumber) => {
     var breakpoint_node = document.createElement('span');
-    breakpoint_node.innerHTML = '&#128308;';
-    breakpoint_node.style.fontSize = '10px';
+    breakpoint_node.className = 'breakpoint-node';
+    breakpoint_node.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 24 24" width="15" fill="currentColor"><path d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zM5 9a1 1 0 1 0 0 2h10a1 1 0 0 0 0-2H5z"></path></svg>';
 
     // Add breakpoint functionality
     add_breakpoint(lineNumber);
@@ -90,7 +92,7 @@ const CodeBox = ({ code, setCode, line }) => {
           gutters: [breakpointGutterID, lineMarkerGutterID, 'CodeMirror-linenumbers']
         }}
         onGutterClick={(editor, lineNumber) => {
-          if (lineNumber > 0) handleBreakpoints(editor, lineNumber);
+          handleBreakpoints(editor, lineNumber);
         }}
         editorDidMount={(_editor, _value, next) => {
           setNext(() => next);
