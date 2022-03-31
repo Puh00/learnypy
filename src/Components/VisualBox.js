d9d91fb8808a59f9f2d8d67be1d6de49e4b24a8d
moa114 <bermoa@student.chalmers.se>
2022-03-31 10:23:19 +0200

    text for graph

--- src/Components/VisualBox.js
+++ src/Components/VisualBox.js
@@ -3,24 +3,29 @@ import React, { useEffect, useState } from 'react';
 import { graphviz } from 'd3-graphviz';
 import generate_dot from '../util/dotGenerator';
 
+import set_text from '../util/textGenerator';
+
 import styles from './VisualBox.module.css';
 import border from './Border.module.css';
+var text;
 
 const VisualBox = ({ data }) => {
   const [graph, setGraph] = useState({ dot: 'graph {}' });
-
-  //TODO screen reader + tab
+  //TODO tab
 
   useEffect(() => {
     const dot = generate_dot(data);
     setGraph(dot);
+    text = set_text(data.objects, data.variables);
   }, [data]);
 
   useEffect(() => {
     graphviz(`#graph-body`).renderDot(graph.dot);
   }, [graph]);
 
-  return <div className={`${styles.Container} ${border.Border}`} id="graph-body"></div>;
+  return (
+    <div className={`${styles.Container} ${border.Border}`} id="graph-body" aria-label={text}></div>
+  );
 };
 
 export default VisualBox;
