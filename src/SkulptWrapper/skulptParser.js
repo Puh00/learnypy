989d8da3462842db83801df0596d8595941d4e39
Puh00 <alex.phu@live.com>
2022-03-31 15:35:28 +0200

    Refactor global variables out of functions and add documentation

--- src/SkulptWrapper/skulptParser.js
+++ src/SkulptWrapper/skulptParser.js
@@ -1,24 +1,26 @@
 import { v4 as uuidv4 } from 'uuid';
 
-let variables = [];
-let objects = [];
-
-// retrieve the global variables and objects
+/**
+ * Retrieves the global variables and objects of the current Skulpt state
+ * @returns The global variables and objects
+ */
 const parse_globals = () => parse_objects(window.Sk.globals);
 
-// retrieve the local variables and objects
+/**
+ * Retrieves the local variables and objects of the current Skulpt state
+ * @returns The local variables and objects
+ */
 const parse_locals = () => parse_objects(window.Sk._frame?.locals);
 
 /**
  * Parse the current status of {other} and return variables and objects
- * @param {Object} other generally globals or locals
- * @param {Array} filter irrelevant python attributes that are filtered out
- * @returns variables and objects from {other}
+ * @param {Object} other Generally globals or locals
+ * @param {Array} filter Irrelevant python attributes that are filtered out
+ * @returns Variables and objects from {other}
  */
 const parse_objects = (other, filter = ['__doc__', '__file__', '__name__', '__package__']) => {
-  // Variables and objects reset before parsing
-  variables = [];
-  objects = [];
+  const variables = [];
+  const objects = [];
 
   // there's a risk for locals being undefined
   if (other) {
@@ -31,7 +33,7 @@ const parse_objects = (other, filter = ['__doc__', '__file__', '__name__', '__pa
 
       variables.push({
         name: key,
-        ref: retrieve_object_id(value)
+        ref: retrieve_object_id(objects, value)
       });
     }
   }
@@ -42,17 +44,19 @@ const parse_objects = (other, filter = ['__doc__', '__file__', '__name__', '__pa
   };
 };
 
-/*
-Create and return new object.
-Type of object:
-  Object: {
-    id: uuid for the object,
-    value: used for visual representation,
-    type: type of the object,
-    js_object: used to compare with objects from Sk.globals
-  }
-*/
-const create_object = (js_object) => {
+/**
+ * Creates an object of the follwing structure and appends it to the list of objects:
+ * Object: {
+ *   id: uuid for the object,
+ *   value: used for visual representation,
+ *   type: type of the object,
+ *   js_object: used to compare with objects from Sk.globals
+ * }
+ * @param {Array} objects List of the parsed objects
+ * @param {Object} js_object The Javascript representation of the Python object
+ * @returns The newly created object
+ */
+const create_object = (objects, js_object) => {
   let obj = {
     id: uuidv4(),
     value: null,
@@ -71,9 +75,8 @@ const create_object = (js_object) => {
     value = [];
     const entries = Object.values(js_object.entries);
     for (const entr of entries) {
-      value.push({ key: entr.lhs.v, val: retrieve_object_id(entr.rhs) });
+      value.push({ key: entr.lhs.v, val: retrieve_object_id(objects, entr.rhs) });
     }
-
     // Add more types here
   } else {
     // Immutables
@@ -84,34 +87,40 @@ const create_object = (js_object) => {
   return obj;
 };
 
-// Retrieve the object id for the given value.
-// If the value already exists the id to the already existing object is returned.
-// Otherwise a new object is created and this id is returned.
-const retrieve_object_id = (js_object) => {
+/**
+ * Retrieves the object id for the given value. If an object with this value already
+ * exists then their id is returned. Otherwise a new object is created and this object's
+ * id is returned.
+ * @param {Array} objects List of the parsed objects
+ * @param {Object} js_object The Javascript representation of the Python object
+ * @returns The id of the object with a matching value
+ */
+const retrieve_object_id = (objects, js_object) => {
   var obj;
   for (const obj of objects) {
     // '===' returns true only if the objects have the same reference
-    if (obj.js_object === js_object) {
-      return obj.id;
-    }
+    if (obj.js_object === js_object) return obj.id;
   }
   //small int objects is not created more than once
   if (js_object.tp$name === 'int' && js_object.v <= 256 && js_object.v >= -5) {
-    var small_int_id = retrieve_small_int_object_id(js_object, objects);
-    if (!small_int_id) {
-      obj = create_object(js_object);
-    } else return small_int_id;
-  } else {
-    // If the object doesn't exist yet add it and return new id
-    obj = create_object(js_object);
+    var small_int_id = retrieve_small_int_object_id(objects, js_object);
+    if (!small_int_id) obj = create_object(objects, js_object);
+    else return small_int_id;
   }
-  //objects.push(obj);
+  // If the object doesn't exist yet add it and return new id
+  else obj = create_object(objects, js_object);
+
   return obj.id;
 };
 
-// If the small int value already exists the id to the already existing object is returned,
-// otherwise false is returned.
-const retrieve_small_int_object_id = (js_object) => {
+/**
+ * If a small int value already exists then the id to the already existing object is
+ * returned, otherwise false is returned
+ * @param {Array} objects List of the parsed objects
+ * @param {Object} js_object The Javascript representation of the Python object
+ * @returns id or false.
+ */
+const retrieve_small_int_object_id = (objects, js_object) => {
   for (const obj of objects) {
     if (obj.type === 'int' && obj.js_object.v === js_object.v) {
       return obj.id;
