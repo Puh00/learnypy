8c9beb371a5289109f53c0a64ab729ce0d5d46b5
yenanw <54945977+yenanw@users.noreply.github.com>
2022-04-01 08:56:46 +0200

    move tests to own folder

---
+++ src/tests/graph_text.test.js
@@ -0,0 +1,1 @@
+test('assignment with integers', () => {});
--- src/tests/references.test.js
+++ src/tests/references.test.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from './App';
-import sleep from './util/sleep';
-import { getObjectById, getVariableByName, getRefs } from './util/testUtils';
+import App from '../App';
+import sleep from '../util/sleep';
+import { getObjectById, getVariableByName, getRefs } from '../util/testUtils';
 
 // mock these components since the imported libraries seem to break everything...
-jest.mock('./Components/VisualBox', () => {
+jest.mock('../Components/VisualBox', () => {
   return function VisualBox({ data }) {
     // js_object contains raw javascript object which makes it impossible to
     // stringify using JSON
@@ -19,7 +19,7 @@ jest.mock('./Components/VisualBox', () => {
   };
 });
 
-jest.mock('./Components/CodeBox', () => {
+jest.mock('../Components/CodeBox', () => {
   return function CodeBox({ code, setCode }) {
     return (
       <form className="Code-box">
