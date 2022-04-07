ddfec80796d4a73955ca305563414a03bd68b097
yenanw <54945977+yenanw@users.noreply.github.com>
2022-04-07 13:01:55 +0200

    remove title texts after graph render to prevent tooltips

--- src/Components/VisualBox.js
+++ src/Components/VisualBox.js
@@ -1,5 +1,5 @@
 import { graphviz } from 'd3-graphviz';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import generate_dot from '../util/dotGenerator';
 import set_text from '../util/textGenerator';
@@ -10,6 +10,7 @@ var text;
 
 const VisualBox = ({ data }) => {
   const [graph, setGraph] = useState({ dot: 'graph {}' });
+  const container = useRef(null);
 
   useEffect(() => {
     const dot = generate_dot(data);
@@ -18,8 +19,19 @@ const VisualBox = ({ data }) => {
   }, [data]);
 
   useEffect(() => {
+    const removeTitle = (ele) => {
+      if (ele.tagName === 'title') {
+        console.log(ele);
+        ele.innerHTML = '';
+        return;
+      }
+
+      ele.childNodes.forEach((node) => {
+        removeTitle(node);
+      });
+    };
+
     graphviz(`#graph-body`)
-      .keyMode('title') // default keyMode is title, switch to id to avoid bugs
       .attributer((d) => {
         if (d.tag === 'svg') {
           // hide the generated svg file from screen readers
@@ -28,10 +40,15 @@ const VisualBox = ({ data }) => {
         }
       })
       .renderDot(graph.dot);
+
+    // remove title texts from all <title> tags, to prevent tooltips
+    // after the graph has been rendered, this is a kinda scuffed...
+    removeTitle(container.current.childNodes[0]);
   }, [graph]);
 
   return (
     <div
+      ref={container}
       className={`${styles.Container} ${border.Border}`}
       id="graph-body"
       tabIndex={0}
