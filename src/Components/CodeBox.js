7c4579e03778810bb3ebcf5a27b92f0193b8cf20
yen <54945977+yenanw@users.noreply.github.com>
2022-04-12 16:42:03 +0200

    disable multi cursor functionality (#78)
    Co-authored-by: Phu <54896147+Puh00@users.noreply.github.com>

--- src/Components/CodeBox.js
+++ src/Components/CodeBox.js
@@ -97,6 +97,9 @@ const CodeBox = ({
       <CodeMirror
         value={code}
         options={{
+          configureMouse: () => {
+            return { addNew: false };
+          },
           lineWrapping: true,
           lint: true,
           mode: 'python',
