2f1a353263be5e709edb26db491906627e944b6a
Puh00 <alex.phu@live.com>
2022-05-30 23:43:12 +0200

    Rephrase

--- src/pages/About.js
+++ src/pages/About.js
@@ -70,7 +70,7 @@ const About = () => {
           <li>Many iterations will make the website slow.</li>
           <li>Only Python 3.7.3 grammar is supported.</li>
           <li>A few Python libraries are supported.</li>
-          <li>Debugger is unable to suspend correctly when importing Python modules.</li>
+          <li>Debugger is unable to step correctly when importing some Python modules.</li>
         </ul>
         <h2 className={styles.Title}>Special Thanks</h2>
         <h3 className={styles.SubTitle}> Skulpt </h3>
